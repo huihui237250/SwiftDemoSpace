da61e7743e7e047c05bc7863ecf546c4021241a9
TZCPLiOS <banning.cai@tengzhangroup.com>
2021-04-28 09:30:32 +0800

    test

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+print("Hello, playground!")
+
+let myVariable = 42
+var myVariable2 = 50
+print("\(myVariable) + \(myVariable2) = \(myVariable + myVariable2)")
+
+let height : Float = 5.0
+print("\(height)")
+
+let label = "The width is"
+let width = 94
+let widthLabel = label + " " + String(width)
+
+let apples = 3
+let orangers = 5
+let appleSummary = "I have \(apples) apples"
+let fruitSummary = "I have \(apples + orangers) pieces of fruit."
+
+
+
